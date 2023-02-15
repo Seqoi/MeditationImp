c4e0af08ba56cc1bb20b1379fb49993c87549154
sleepsemek <bayukovvdaniil@gmail.com>
2023-02-15 20:03:16 +0300

    Meditation w/ Google Firebase API registration

--- app/src/main/java/com/example/meditation/PageFragmentSounds.java
+++ app/src/main/java/com/example/meditation/PageFragmentSounds.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import java.lang.reflect.Field;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
@@ -27,20 +28,8 @@ import java.util.concurrent.TimeUnit;
 
 public class PageFragmentSounds extends Fragment {
 
-    public static final String[] audio_names = {
-            "Darko US - Infinite Beauty",
-            "Newcomer - 2031 A NEW YORK ODYSSEY",
-            "Newcomer - ONCE UPON AUTUMN APRIL, BEGINS ITS WINTER MAY",
-            "Sleep Token - High Water",
-            "Blind Channel - Left Outside Alone"
-    };
-    public static final Integer[] audio_ids = {
-            R.raw.infinitebeauty,
-            R.raw.song2,
-            R.raw.song3,
-            R.raw.song4,
-            R.raw.song5
-    };
+    //TODO: Для работы плеера нужно переместить заботливо удаленные мной аудиозаписи в папку raw проекта в формате mp3!
+
     private RecyclerView recyclerView;
     private MusicAdapter adapter;
 
@@ -68,9 +57,8 @@ public class PageFragmentSounds extends Fragment {
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
-        adapter = new MusicAdapter(getActivity().getApplicationContext(), prepareData());
+        adapter = new MusicAdapter(getActivity().getApplicationContext(), fillMusic());
         recyclerView.setAdapter(adapter);
-
     }
 
     public class CreateList {
@@ -84,42 +72,29 @@ public class PageFragmentSounds extends Fragment {
         public Integer getAudio_ID() {
             return audio_id;
         }
-        public String getAudio_duration() { return audio_duration; }
 
         public void setAudio_title(String android_version_name) { this.audio_title = android_version_name; }
         public void setAudio_duration(String android_audio_duration) { this.audio_duration = android_audio_duration; }
         public void setAudio_ID(Integer android_audio_url) { this.audio_id = android_audio_url; }
     }
 
-    public String calcDuration(int i) {
-        MediaPlayer mediaPlayer;
-        Long TimeDurationMinutes;
-        Long TimeDurationSeconds;
-        String TimeDuration;
-
-        mediaPlayer = MediaPlayer.create(getView().getContext(), audio_ids[i]);
-        TimeDurationMinutes = TimeUnit.MILLISECONDS.toMinutes(mediaPlayer.getDuration());
-        TimeDurationSeconds = TimeUnit.MILLISECONDS.toSeconds(mediaPlayer.getDuration()) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(mediaPlayer.getDuration()));mediaPlayer.release();
-        TimeDuration = String.format("%02d:%02d", TimeDurationMinutes, TimeDurationSeconds);
-        mediaPlayer.release();
-
-        return TimeDuration;
-    }
-
-    private ArrayList<CreateList> prepareData() {
-        ArrayList<CreateList> theaudio = new ArrayList<>();
-        for(int i = 0; i < audio_ids.length; i++){
-            CreateList createList = new CreateList();
-            createList.setAudio_title(audio_names[i]);
-            createList.setAudio_ID(audio_ids[i]);
-            createList.setAudio_duration(calcDuration(i));
-            theaudio.add(createList);
-        }
-        return theaudio;
-    }
-
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.profile, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private ArrayList<CreateList> fillMusic() {
+        ArrayList<CreateList> list = new ArrayList<>();
+        Field[] fields = R.raw.class.getFields();
+        for (Field f : fields)
+            try {
+                CreateList createList = new CreateList();
+                createList.setAudio_ID(f.getInt(null));
+                createList.setAudio_title(f.getName());
+                list.add(createList);
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        return list;
     }
 }
